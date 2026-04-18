9a21e7f5afdf1463baf297b38aad82808d590e80
Gungunverma1227 <gungunverma9310@gmail.com>
2026-04-19 01:30:42 +0530

    deleted all files

--- routes/fileRoutes.js
+++
@@ -1,9 +0,0 @@
-const express = require("express");
-const router = express.Router();
-const { uploadFile } = require("../controllers/fileController");
-const { protect } = require("../middlewares/authMiddleware");
-const upload = require("../middlewares/uploadMiddleware");
-
-router.post("/upload", protect, upload.single("file"), uploadFile);
-
-module.exports = router;
